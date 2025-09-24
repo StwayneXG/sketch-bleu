d80827fe128f014b573e8d15b4729d7119b56330
Irtaza Sajid Qureshi <stwaynexg@gmail.com>
2025-09-24 01:18:20 -0400

    error handling

--- codebleu/parser/utils.py
+++ codebleu/parser/utils.py
@@ -16,31 +16,35 @@ def remove_comments_and_docstrings(source, lang):
         prev_toktype = tokenize.INDENT
         last_lineno = -1
         last_col = 0
-        for tok in tokenize.generate_tokens(io_obj.readline):
-            token_type = tok[0]
-            token_string = tok[1]
-            start_line, start_col = tok[2]
-            end_line, end_col = tok[3]
-            tok[4]
-            if start_line > last_lineno:
-                last_col = 0
-            if start_col > last_col:
-                out += " " * (start_col - last_col)
-            # Remove comments:
-            if token_type == tokenize.COMMENT:
-                pass
-            # This series of conditionals removes docstrings:
-            elif token_type == tokenize.STRING:
-                if prev_toktype != tokenize.INDENT:
-                    # This is likely a docstring; double-check we're not inside an operator:
-                    if prev_toktype != tokenize.NEWLINE:
-                        if start_col > 0:
-                            out += token_string
-            else:
-                out += token_string
-            prev_toktype = token_type
-            last_col = end_col
-            last_lineno = end_line
+        try:
+            for tok in tokenize.generate_tokens(io_obj.readline):
+                token_type = tok[0]
+                token_string = tok[1]
+                start_line, start_col = tok[2]
+                end_line, end_col = tok[3]
+                tok[4]
+                if start_line > last_lineno:
+                    last_col = 0
+                if start_col > last_col:
+                    out += " " * (start_col - last_col)
+                # Remove comments:
+                if token_type == tokenize.COMMENT:
+                    pass
+                # This series of conditionals removes docstrings:
+                elif token_type == tokenize.STRING:
+                    if prev_toktype != tokenize.INDENT:
+                        # This is likely a docstring; double-check we're not inside an operator:
+                        if prev_toktype != tokenize.NEWLINE:
+                            if start_col > 0:
+                                out += token_string
+                else:
+                    out += token_string
+                prev_toktype = token_type
+                last_col = end_col
+                last_lineno = end_line
+        except Exception:
+            print("tokenize error for this code:\n", source)
+            raise
         temp = []
         for x in out.split("\n"):
             if x.strip() != "":
