3994250afd77a27c2cff39e782f95b0c5f0c539f
Irtaza Sajid Qureshi <stwaynexg@gmail.com>
2025-10-13 18:19:40 -0400

    .

--- codebleu/codebleu.py
+++ codebleu/codebleu.py
@@ -370,8 +370,8 @@ def calc_dataflow_match(reference_sources: List[str], prediction_sources: List[s
     start_time_df_similarity = time.time()
     SIMILARITY_THRESHOLD = 0.3
     from pympler import asizeof
-    for i, ref_dfg in enumerate(ref_dfgs_normalized[:5001]):
-        for j, hyp_dfg in enumerate(hyp_dfgs_normalized[:5001]):
+    for i, ref_dfg in enumerate(ref_dfgs_normalized[:20001]):
+        for j, hyp_dfg in enumerate(hyp_dfgs_normalized[:20001]):
             if i % 1000 == 0 and j % 1000 == 0:
                 logging.debug(f"Computing dataflow similarity for ref_dfg index {i} and hyp_dfg index {j}")
                 memory_info = process.memory_info()
